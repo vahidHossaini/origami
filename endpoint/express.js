981572897fbaf392fd478c2b389a9c2de4172fca
vahid <vahid.hossaini@gmail.com>
2019-08-13 22:21:25 +0430

    download stream

--- endpoint/express.js
+++ endpoint/express.js
@@ -356,6 +356,40 @@ module.exports=class newExpr
         }
         return drivers
     }
+    getStream(path,type,res,req)
+    {
+        const stat = fs.statSync(path)
+        const fileSize = stat.size
+        const range = req.headers.range
+        if (range)
+        { 
+            const parts = range.replace(/bytes=/, "").split("-")
+            const start = parseInt(parts[0], 10)
+            const end = parts[1] 
+              ? parseInt(parts[1], 10)
+              : fileSize-1
+            const chunksize = (end-start)+1
+            const file = fs.createReadStream(path, {start, end})
+            const head = {
+              'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+              'Accept-Ranges': 'bytes',
+              'Content-Length': chunksize,
+              'Content-Type': 'video/mp4',
+            }
+            res.writeHead(206, head);
+            file.pipe(res);
+        }
+        else
+        {
+            const head = {
+                'Content-Length': fileSize,
+                'Content-Type': type,
+              }
+              res.writeHead(200, head)
+              fs.createReadStream(path).pipe(res)
+
+        }
+    }
     constructor(config,dist)
     {
         var self=this
@@ -435,6 +469,11 @@ module.exports=class newExpr
                         }
                         return  self.sendData(self,res,200,data1)
                     })
+                    return
+                }
+                if(dd && dd.streamFileDownload)
+                {
+                    this.getStream(dd.streamFileDownload,dd.type,res,req)
                     return
                 }
   
